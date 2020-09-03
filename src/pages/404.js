86a170025d9559624b7f45014f6c471269bb968a
Zach Thomas <ZThomas@ad.uqunion.uq.edu.au>
2020-09-03 17:58:17 +1000

    Made a dummy Gatsby site

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function FourZeroFour() {
+  return (
+    <p>
+      Damn man, this sucks. You've hit a 404 bro. This really sucks. I'm sorry
+      that happened to you bro. Damn this sucks.
+    </p>
+  )
+}
