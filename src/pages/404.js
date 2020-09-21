0c3a334fe50d88868ea696563d2e1cccad7977ee
Zach <zachary.thomas@uq.net.au>
2020-09-22 01:10:43 +1000

    Updated environment and 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Header from "../components/header"
+
+export default function FourZeroFour() {
+  return (
+    <Layout>
+      <Header headerText="404 Not Found."/>
+      <p>
+        Damn man, this sucks. You've hit a 404 bro. This really sucks. I'm sorry
+        that happened to you bro. Damn this sucks.
+      </p>
+      <p>
+        Hey you know what sometimes works for me? Checking that you repeated the
+        same thing in the directory. For example its not 
+        "zacharythomas.me/contact" it's "zacharythomas.me/contact/contact". I 
+        know it seems stupid! But I promise there's a very sensible reason for 
+        the redundancy. 
+      </p>
+      <p>
+        No good? Damn man...this really sucks. 
+      </p>
+    </Layout>
+  )
+}
