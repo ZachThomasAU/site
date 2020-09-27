8fff8c6f65f5244e0b49a6eed618ec3d03ed201b
Zach <zachary.thomas@uq.net.au>
2020-09-27 11:27:07 +1000

    Optimised performance

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children }) {
     <div
       css={css`
         margin: 0 auto;
-        max-width: 700px;
+        max-width: 1080px;
         padding: ${rhythm(2)};
         padding-top: ${rhythm(1.5)};
       `}
