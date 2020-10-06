bfe2e56676a9beb2972c40668b9dc07728f57f08
Zach <zachary.thomas@uq.net.au>
2020-10-06 23:14:21 +1000

    Robots.txt v5
    Okay so this one works. Guaranteed. This should have been my second
    attempt (attempt 1 and 4 being my 1st)

--- robots-txt.config.js
+++ robots-txt.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   host: "https://zacharythomas.me",
   sitemap: "https://zacharythomas.me/sitemap.xml",
-  policy: [{ userAgent: "*", disallow: "/404", disallow: "/leaguepedia-api/" }],
+  policy: [{ userAgent: "*", disallow: ["/404", "/leaguepedia-api/"] }],
 }
