8c9f9f39757f3e21b49b79e0feeade6923271157
Zach <zachary.thomas@uq.net.au>
2020-10-06 23:03:03 +1000

    robots.txt v3
    Lets go, third times the charm. Why can't the docs just not suck?

--- robots-txt.config.js
+++ robots-txt.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   host: "https://zacharythomas.me",
   sitemap: "https://zacharythomas.me/sitemap.xml",
-  policy: [
-    { userAgent: "*", disallow: "/404" },
-    { userAgent: "*", disallow: "/leaguepedia-api/" },
-  ],
+  policy: [{ userAgent: "*", disallow: "/404, /leaguepedia-api/" }],
 }
