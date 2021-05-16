7bca09556b7d1fc975fc208d0af02efe89ca4f0a
Zachary Thomas <git@zacharythomas.me>
2021-05-16 19:37:13 +1000

    Began a word of the day list. Don't know what my plan is, but I might use it for something.

---
+++ src/utils/word-of-the-day.js
@@ -0,0 +1,65 @@
+const wordOfTheDay = {
+  0: {
+    1: null,
+    3: {
+      'word': "Gainsay",
+      'definition': "Verb; To deny or contradict (especially of a fact or statement)",
+      'origin': "From English, probably an agglutination of the archaic definition of 'gain' (against) and 'say'. Very Anglo"
+    },
+    9: {
+      'word': "Prognosticate",
+      'definition': "Verb; Foretell or prophesy a future event",
+      'origin': "From Medieval Latin, 'prognosticare', to make a prediction",
+    },
+    14: {
+      'word': "Estivate",
+      'definition': "Verb; (Especially of an insect, fish or amphibian) To spend a hot or dry period in a prolonged state or torpor or dormancy",
+      'origin': "From Classical Latin, 'aestivare', to spend (or pass) the summer",
+    },
+  },
+  1: {
+    1: null,
+  },
+  2: {
+    1: null,
+  },
+  3: {
+    1: null,
+    2: {
+      'word': "Agglutinate",
+      'definition': "Verb; To combine different word elements to express compound ideas",
+      'origin': "From Classical Latin, the agglutination of 'ad-' (to) and 'gluten' (glue), meaning to glue",
+    },
+  },
+  4: {
+    1: null,
+    15: {
+      'word': "Vituperative",
+      'definition': "Adj; Bitter and abusive",
+      'definition': "From Classical Latin, 'vituperare', to blame",
+    },
+  },
+  5: {
+    1: null,
+  },
+  6: {
+    1: null,
+  },
+  7: {
+    1: null,
+  },
+  8: {
+    1: null,
+  },
+  9: {
+    1: null,
+  },
+  10: {
+    1: null,
+  },
+  11: {
+    1: null,
+  },
+};
+
+export default wordOfTheDay;
